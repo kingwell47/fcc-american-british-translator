905d45ca8e39d441f9b59e70f602bf23e214a395
kingwell47 <joeldoctor47@gmail.com>
2021-11-16 18:22:32 +0800

    implemented rudimentary translation

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,36 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  americanToBritish(word) {
+    if (americanOnly[word]) {
+      return americanOnly[word];
+    } else if (americanToBritishSpelling[word]) {
+      return americanToBritishSpelling[word];
+    } else if (americanToBritishTitles[word]) {
+      return americanToBritishTitles[word];
+    } else {
+      return false;
+    }
+  }
 
+  getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+  }
+
+  britishToAmerican(word) {
+    if (britishOnly[word]) {
+      return britishOnly[word];
+    } else if (this.getKeyByValue(americanToBritishSpelling, word)) {
+      return this.getKeyByValue(americanToBritishSpelling, word);
+    } else if (this.getKeyByValue(americanToBritishTitles, word)) {
+      return this.getKeyByValue(americanToBritishTitles, word);
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,19 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+    switch (locale) {
+      case "american-to-british":
+        console.log(translator.americanToBritish(text));
+        break;
+      case "british-to-american":
+        console.log(translator.britishToAmerican(text));
+        break;
+    }
+  });
 };
